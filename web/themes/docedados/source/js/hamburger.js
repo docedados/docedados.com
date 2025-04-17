6348c532203956c62ddefafdc26be60d15761bcd
neverbot <ivan@neverbot.com>
2025-04-17 19:03:45 +0200

    chore: some more work in header

---
+++ web/themes/docedados/source/js/hamburger.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const hamburger = document.querySelector('.hamburger');
+  const nav = document.querySelector('.header-nav');
+
+  hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('is-active');
+    nav.classList.toggle('is-active');
+  });
+});
