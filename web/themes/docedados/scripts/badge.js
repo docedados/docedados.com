00e3fa8e0b4a29dde51d1383f50af13662c83a4c
neverbot <ivan@neverbot.com>
2025-05-22 20:06:08 +0200

    Add hexo-docedados-theme as submodule

--- web/themes/docedados/scripts/badge.js
+++
@@ -1,5 +0,0 @@
-hexo.extend.tag.register('badge', function (args) {
-  const [color, content] = args
-
-  return '<span class="badge ' + color + '">' + content + '</span>'
-}, { ends: false, async: false })
--- web/themes/docedados/source/js/hamburger.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const hamburger = document.querySelector('.hamburger');
-  const nav = document.querySelector('.header-nav');
-
-  hamburger.addEventListener('click', () => {
-    hamburger.classList.toggle('is-active');
-    nav.classList.toggle('is-active');
-  });
-});
